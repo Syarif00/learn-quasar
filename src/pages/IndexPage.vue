6545960a2350f0ce9a6e5ea896629bc0090bc9fd
suriptw <achmadsyarif000@gmail.com>
2024-05-31 08:25:48 +0700

    first

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-mt-md">version v{{ $q.version }}</div>
+    <div class="q-mt-md">platform v{{ $q.platform }}</div>
+    <div class="q-mt-md">lang {{ $q.lang }}</div>
+    <div class="q-mt-md">iconSet v{{ $q.iconSet }}</div>
+    <div class="q-mt-md">cordova v{{ $q.cordova }}</div>
+    <div class="q-mt-md">capacitor v{{ $q.capacitor }}</div>
+    <q-btn color="warning">cek kali ya</q-btn>
+  </div>
+</template>
